92af1b4769a1d00ffeaf3407b4926759f2c0d0e5
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2025-03-26 19:24:53 +0300

    ReactTempladeAdd

---
+++ OmnIDEApp/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ConfigScreen from './ConfigScreen';
+
+function App() {
+    return (
+        <div className="App">
+            <ConfigScreen />
+        </div>
+    );
+}
+
+export default App;
---
+++ OmnIDEApp/src/ConfigScreen.js
@@ -0,0 +1,13 @@
+// src/ConfigScreen.js
+import React from 'react';
+
+const ConfigScreen = () => {
+    return (
+        <div>
+            <h1>IDE Configuration Screen</h1>
+            {/* Burada konfigürasyon bileşenleri ekleyebilirsiniz */}
+        </div>
+    );
+};
+
+export default ConfigScreen;
