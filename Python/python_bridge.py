03837d4b6d7fd02a1e41dcb52741522aa1b957f6
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2025-04-22 21:06:42 +0300

    Python_Bridge pythonside added
    still not working

---
+++ Python/python_bridge.py
@@ -0,0 +1,18 @@
+import DataProcessor
+import CodeExecutor
+import ReportGenerator
+
+class PythonBridge:
+    def __init__(self):
+        self.data_processor = DataProcessor()
+        self.code_executor = CodeExecutor()
+        self.report_generator = ReportGenerator()
+
+    def call_data_processor(self, files):
+        return self.data_processor.process_zip(files)
+
+    def call_code_executor(self, files, metadata):
+        return self.code_executor.run_code(files, metadata)
+
+    def call_report_generator(self, results):
+        return self.report_generator.generate_report(results)
